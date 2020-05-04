523f5da34284b2ca1577a72ea6514db991745406
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-04 17:39:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  let a = animal
-  return a
+  return animal
 }
 
 function add2(n) {
